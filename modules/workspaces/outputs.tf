822feff352ab5076f4428705581febef0fb2e28b
Hugo Samayoa <htplbc@gmail.com>
2020-09-29 17:34:42 -0700

    Clean up outputs and versions from workspaces module

--- modules/workspaces/outputs.tf
+++ modules/workspaces/outputs.tf
@@ -1,33 +1,9 @@
-output "top_level_id" {
+output "id" {
   description = "The workspace's human-readable ID, which looks like `<ORGANIZATION>/<WORKSPACE>`."
-  value       = tfe_workspace.top_level.id
+  value       = tfe_workspace.this.id
 }
 
-output "workspace_ids" {
-  description = "The workspace's human-readable ID, which looks like `<ORGANIZATION>/<WORKSPACE>`."
-  value = local.workspace_ids
-}
-
-output "workspace_configuration" {
-  value = local.workspace_configuration
-}
-
-output "globals_configuration" {
-  value = local.workspace_globals
-}
-
-output "projects_configuration" {
-  value = local.workspace_projects
-}
-
-output "workspace_globals" {
-  value = local.id_globals_map
-}
-
-output "workspace_variables" {
-  value = local.id_variables_map
-}
-
-output "workspace_triggers" {
-  value = local.id_triggers_map
+output "name" {
+  description = "The workspace's name"
+  value       = tfe_workspace.this.nameß
 }
