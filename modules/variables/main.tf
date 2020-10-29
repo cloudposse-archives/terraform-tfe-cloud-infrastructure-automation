f6193417c85cef4ed1c5f57f6b8bfbdf7d204aa4
Dan Meyers <20687081+danjbh@users.noreply.github.com>
2020-10-28 18:09:10 -0700

    Updating comment in modules/variables/main.tf
    Co-authored-by: Erik Osterman <erik@cloudposse.com>

--- modules/variables/main.tf
+++ modules/variables/main.tf
@@ -5,6 +5,7 @@ resource "tfe_variable" "this" {
   hcl          = var.hcl
   key          = var.category == "env" ? "TF_VAR_${each.key}" : each.key
   sensitive    = var.sensitive
+  # translate a JSON encoded string to proper HCL2
   value        = var.hcl ? replace(replace(jsonencode(each.value), "/(\".*?\"):/", "$1 = "), "/= null/", "= \"\"") : jsonencode(each.value)
   workspace_id = var.workspace_id
 }
