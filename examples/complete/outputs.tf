01ca558c2451d484d962bd72b46deebbc2f11ed0
Maxim Mironenko <maxim@cloudposse.com>
2021-01-11 19:24:48 +0700

    tests triage

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -10,10 +10,10 @@ output "global_workspace_id" {
 
 output "environment_workspaces_ids" {
   description = "Environment workspaces ids"
-  value       = module.example.environment_workspaces[*].id
+  value       = values(module.example.environment_workspaces)[*].id
 }
 
 output "project_workspaces_ids" {
   description = "Environment workspaces ids"
-  value       = module.example.project_workspaces[*].id
+  value       = values(module.example.project_workspaces)[*].id
 }
